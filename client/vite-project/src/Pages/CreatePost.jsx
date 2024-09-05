a4a936142f274728e98ef87a6b417a7f22007084
Snehasish <snehasishmohanty9439@gmail.com>
2024-09-06 00:53:21 +0530

    Added-react-quill

--- client/vite-project/src/Pages/CreatePost.jsx
+++ client/vite-project/src/Pages/CreatePost.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  
   return (
-    <div>Here you create new post</div>
-  )
-}
+    <div className="createpost">
+      <form>
+        <input type="text" placeholder={"Title"} />
+        <input type="text" placeholder={"Summary"} />
+        <input type="file" place />
+        <ReactQuill value={content} modules={modules} formats={formats}/>
+        <button style={{ marginTop: "10px" }}>Post</button>
+      </form>
+    </div>
+  );
+};
 
 export default CreatePost;
