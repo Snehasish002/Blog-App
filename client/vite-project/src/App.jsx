06d23ef9fc92fee9d0e0e5e9653d4709cc79b7f1
Snehasish <snehasishmohanty9439@gmail.com>
2024-09-07 03:31:39 +0530

    created-post-page

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/create" element={<CreatePost/>}/>
+            {/* <Route path="/post/:id" element/> */}
         </Route>
       </Routes>
     </UserContextProvider>
--- client/vite-project/src/Post.jsx
+++ client/vite-project/src/Post.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import { format } from "date-fns";
 
-const Post = ({ title, summary, cover, content, createdAt, author }) => {
+const Post = ({ _id, title, summary, cover, content, createdAt, author }) => {
   return (
     <div className="post">
       <div className="image">
-        <img
-          src={'http://localhost:3000/'+cover}
-          alt=""
-        />
+        <Link to={`/post/${_id}`}>
+          <img src={"http://localhost:3000/" + cover} alt="" />
+        </Link>
       </div>
 
       <div className="texts">
-        <h2>{title}</h2>
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
         <p className="info">
           <a className="author">{author.username}</a>
-          <time>{format(new Date(createdAt),'MMM d, yyyy hh:mm aaa')}</time>
+          <time>{format(new Date(createdAt), "MMM d, yyyy hh:mm aaa")}</time>
         </p>
         <p className="summary">{summary}</p>
       </div>
